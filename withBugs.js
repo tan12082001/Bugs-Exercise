dcf1a54a2bdaca3fc3b232159e459472c3898630
Tanmayi Manku <81354942+tan12082001@users.noreply.github.com>
2023-06-23 20:32:39 +0530

    Add bugs to withBugs.js

--- withBugs.js
+++ withBugs.js
@@ -1,1 +1,21 @@
-console.log('Hello world);
+// Exercise - Find the Bugs present in this JS file using the developer tools in the browser.
+let number = 20;
+let a = 2;
+b = 4;
+let array = [];
+for(let i = 0; i < number; i += 1 ) {
+  console.log(`The double of the number ${j} is ${j*2}`);
+  array.push(i*2);
+}
+
+function sum (a, b) {
+  return a+b;
+}
+
+function sum (a, b, c) {
+  return a + b * 2;
+}
+}
+console.log( `the sum of the numbers ${a} and ${b} is ${sum(a, b)}` );
+console.log( `the double of the sum of the numbers ${a} and ${b} is ${sum(a, b, c)}` );
+console.log( `the last value in the array ${array} is ${array[array.length]}.` );
